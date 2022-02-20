efadeb162dfbeb0598756b83c82bc4a0ef3f9a48
Oğuzhan Bay <oguzhan.bay@diyalog.im>
2022-02-20 13:49:23 +0300

    create-po page tabs component added

--- pages/orders/create-po/index.js
+++ pages/orders/create-po/index.js
@@ -6,7 +6,11 @@ import Card from '../../../components/antdform/card/card';
 import Input from '../../../components/antdform/input/input';
 import FormItem from '../../../components/antdform/form-item/FormItem';
 import { clients, composition, currency, departments, fabriccode, paymentTerm, seasons } from '../../../constant/constant';
-import { DatePicker, Space } from 'antd';
+import { DatePicker, Divider, Space } from 'antd';
+import { Checkbox } from 'antd';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 export default function CreatePo() {
   const {
@@ -20,6 +24,10 @@ export default function CreatePo() {
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  function callback(key) {
+    console.log(key);
+  }
   const watchClientSelection = watch('clients');
   const watchSeasonsSelection = watch('seasons');
   const watchDepartmentSelection = watch('departments');
@@ -28,9 +36,7 @@ export default function CreatePo() {
     <Page title={'Create User'}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Card>
-          <fieldset className='mb-6 text-md font-bold'>
-            <legend>Create PO</legend>
-          </fieldset>
+          <Divider orientation='center'>Create PO</Divider>
 
           <Row gutterWidth={16}>
             <Col lg={6}>
@@ -209,113 +215,252 @@ export default function CreatePo() {
             </Col>
           </Row>
         </Card>
-        <Card>
-          <fieldset className='mb-6 text-md font-bold'>
-            <legend>Assignment for Division</legend>
-          </fieldset>
 
-          <Row gutterWidth={16}>
-            <Col lg={3}>
-              <FormItem label='Division Manager ' error={errors?.divisionmanager}>
-                <Controller
-                  name='divisionmanager'
-                  control={control}
-                  render={({ field }) => <Select instanceId='divisionmanager' {...field} options={currency} />}
-                  {...register('divisionmanager', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='Merchandiser ' error={errors?.merchandiser}>
-                <Controller
-                  name='merchandiser'
-                  control={control}
-                  render={({ field }) => <Select instanceId='merchandiser' {...field} options={currency} />}
-                  {...register('merchandiser', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='QC Manager ' error={errors?.qcmanager}>
-                <Controller
-                  name='qcmanager'
-                  control={control}
-                  render={({ field }) => <Select instanceId='qcmanager' {...field} options={currency} />}
-                  {...register('qcmanager', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='QC ' error={errors?.qc}>
-                <Controller
-                  name='qc'
-                  control={control}
-                  render={({ field }) => <Select instanceId='qc' {...field} options={currency} />}
-                  {...register('qc', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='Fit ' error={errors?.fit}>
-                <Controller
-                  name='fit'
-                  control={control}
-                  render={({ field }) => <Select instanceId='fit' {...field} options={currency} />}
-                  {...register('fit', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='Accounting + Traffic ' error={errors?.accountingwithtraffic}>
-                <Controller
-                  name='accountingwithtraffic'
-                  control={control}
-                  render={({ field }) => <Select instanceId='accountingwithtraffic' {...field} options={currency} />}
-                  {...register('accountingwithtraffic', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='Fit Assistant ' error={errors?.fitassistant}>
-                <Controller
-                  name='fitassistant'
-                  control={control}
-                  render={({ field }) => <Select instanceId='fitassistant' {...field} options={currency} />}
-                  {...register('fitassistant', { required: ' required field' })}
-                />
-              </FormItem>
-            </Col>
-          </Row>
-        </Card>
+        <Tabs defaultActiveKey='1' onChange={callback}>
+          <TabPane tab='Assignment for Division' key='1'>
+            <Card>
+              <Row gutterWidth={16}>
+                <Col lg={3}>
+                  <FormItem label='Division Manager ' error={errors?.divisionmanager}>
+                    <Controller
+                      name='divisionmanager'
+                      control={control}
+                      render={({ field }) => <Select instanceId='divisionmanager' {...field} options={currency} />}
+                      {...register('divisionmanager', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='Merchandiser ' error={errors?.merchandiser}>
+                    <Controller
+                      name='merchandiser'
+                      control={control}
+                      render={({ field }) => <Select instanceId='merchandiser' {...field} options={currency} />}
+                      {...register('merchandiser', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='QC Manager ' error={errors?.qcmanager}>
+                    <Controller
+                      name='qcmanager'
+                      control={control}
+                      render={({ field }) => <Select instanceId='qcmanager' {...field} options={currency} />}
+                      {...register('qcmanager', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='QC ' error={errors?.qc}>
+                    <Controller
+                      name='qc'
+                      control={control}
+                      render={({ field }) => <Select instanceId='qc' {...field} options={currency} />}
+                      {...register('qc', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='Fit ' error={errors?.fit}>
+                    <Controller
+                      name='fit'
+                      control={control}
+                      render={({ field }) => <Select instanceId='fit' {...field} options={currency} />}
+                      {...register('fit', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='Accounting + Traffic ' error={errors?.accountingwithtraffic}>
+                    <Controller
+                      name='accountingwithtraffic'
+                      control={control}
+                      render={({ field }) => <Select instanceId='accountingwithtraffic' {...field} options={currency} />}
+                      {...register('accountingwithtraffic', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+                <Col lg={3}>
+                  <FormItem label='Fit Assistant ' error={errors?.fitassistant}>
+                    <Controller
+                      name='fitassistant'
+                      control={control}
+                      render={({ field }) => <Select instanceId='fitassistant' {...field} options={currency} />}
+                      {...register('fitassistant', { required: ' required field' })}
+                    />
+                  </FormItem>
+                </Col>
+              </Row>
+            </Card>
+          </TabPane>
+          <TabPane tab='Dates' key='2'>
+            <Card>
+              <Row gutterWidth={16}>
+                <Col lg={2}>
+                  <FormItem label='Entry Date ' error={errors?.entrydate}>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label='Approval Date ' error={errors?.approvaldate}>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
 
-        <Card>
-          <fieldset className='mb-6 text-md font-bold'>
-            <legend>DATES</legend>
-          </fieldset>
+                <Col lg={2}>
+                  <FormItem label='Supplier Shipment Date ' error={errors?.suppliershipmentdate}>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Shipment Date ' error={errors?.shipmentdate}>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={12}>
+                  <Divider orientation='center'>Approval Deadlines</Divider>
+                </Col>
 
-          <Row gutterWidth={16}>
-            <Col lg={3}>
-              <FormItem label='Entry Date ' error={errors?.entrydate}>
-                <DatePicker />
+                <Col lg={2}>
+                  <FormItem label=' Fabric '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Color '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Fit '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Internal Test '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' International Test '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={1}></Col>
+                <Col lg={1}></Col>
+                <Col lg={2}>
+                  <FormItem label=' Printing '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Size Set '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Pre Pro '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Washing '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={2}>
+                  <FormItem label=' Accessory '>
+                    <DatePicker />
+                  </FormItem>
+                </Col>
+                <Col lg={1}></Col>
+              </Row>
+            </Card>
+          </TabPane>
+          <TabPane tab='Color-Size' key='3'>
+            Color-Size
+          </TabPane>
+          <TabPane tab='Accessories' key='4'>
+            Accessories
+          </TabPane>
+          <TabPane tab='Fitting' key='5'>
+            <Card>
+              <Row>
+                <Col lg={3} className='border-r'>
+                  <h1> Fitting Approval </h1>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Fit / Pps</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Size Set</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Top / Gold</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Prod Sample</span>
+                  </label>
+                </Col>
+                <Col lg={3} className='border-r'>
+                  <h1> Test Approval </h1>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Internal Wash Test</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>International Wash Test</span>
+                  </label>
+                </Col>
+                <Col lg={3} className='border-r'>
+                  <h1> Shipment Approval </h1>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Top Sample</span>
+                  </label>
+                </Col>
+                <Col lg={3}>
+                  <h1> More Approval </h1>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Print-Embroidery</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Wash</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Bom</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>QIMA</span>
+                  </label>
+                  <label className='flex items-center space-x-2 cursor-pointer  '>
+                    <Checkbox className='' />
+                    <span>Hanger</span>
+                  </label>
+                </Col>
+              </Row>
+            </Card>
+          </TabPane>
+          <TabPane tab='Note' key='6'>
+            <Col lg={12}>
+              <FormItem htmlFor='Note' label='Note'>
+                <Input.Textarea rows={4} autoComplete='off' id='note' name='note' type='text' />
               </FormItem>
             </Col>
-            <Col lg={3}>
-              <FormItem label='Approval Date ' error={errors?.approvaldate}>
-                <DatePicker />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label='Supplier Shipment Date ' error={errors?.suppliershipmentdate}>
-                <DatePicker />
-              </FormItem>
-            </Col>
-            <Col lg={3}>
-              <FormItem label=' Shipment Date ' error={errors?.shipmentdate}>
-                <DatePicker />
-              </FormItem>
-            </Col>
-          </Row>
-        </Card>
+          </TabPane>
+          <TabPane tab='Company' key='7'>
+            Company
+          </TabPane>
+        </Tabs>
+
         <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
           Save
         </button>
