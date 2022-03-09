508757c910eb932a6c0c622260e34dd9fde05753
MSI\oguzhan <bayoguzhan95@gmail.com>
2022-03-09 13:20:43 +0300

    certifications,countrries,deliveryterms api functions added

--- functions/settings.js
+++ functions/settings.js
@@ -24,3 +24,43 @@ export const addSeason = async (season) =>
 export const deleteSeason = async (slug) => await axios.delete(`/api/deleteSeason/${slug}`);
 
 export const updateSeason = async (slug, season) => await axios.put(`/api/updateSeason/${slug}`, season);
+
+// Certifications
+
+export const getAllCertifications = async () => await axios.get('/api/getAllCertifications');
+
+export const addCertification = async (certification) =>
+  await axios.post(`/api/addCertification`, {
+    certification,
+  });
+
+export const deleteCertificate = async (slug) => await axios.delete(`/api/deleteCertificate/${slug}`);
+
+export const updateCertification = async (slug, certification) =>
+  await axios.put(`/api/updateCertification/${slug}`, certification);
+
+// Countries
+
+export const getAllCountries = async () => await axios.get('/api/getAllCountries');
+
+export const addCountry = async (country) =>
+  await axios.post(`/api/addCountry`, {
+    country,
+  });
+
+export const deleteCountry = async (slug) => await axios.delete(`/api/deleteCountry/${slug}`);
+
+export const updateCountry = async (slug, country) => await axios.put(`/api/updateCountry/${slug}`, country);
+
+// Delivery Terms
+
+export const getAllDeliveryTerms = async () => await axios.get('/api/getAllDeliveryTerms');
+
+export const addDeliveryTerm = async (deliveryTerm) =>
+  await axios.post(`/api/addDeliveryTerm`, {
+    deliveryTerm,
+  });
+
+export const deleteDeliveryTerm = async (slug) => await axios.delete(`/api/deleteDeliveryTerm/${slug}`);
+
+export const updateDeliveryTerm = async (slug, deliveryTerm) => await axios.put(`/api/updateDeliveryTerm/${slug}`, deliveryTerm);
