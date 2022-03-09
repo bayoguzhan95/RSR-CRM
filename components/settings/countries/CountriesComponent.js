ee1ce643f9be735c5c4a4d9ac0f290f3bd41e8f0
MSI\oguzhan <bayoguzhan95@gmail.com>
2022-03-09 13:38:09 +0300

    Countries Component connected to backend

--- components/settings/countries/CountriesComponent.js
+++ components/settings/countries/CountriesComponent.js
@@ -1,90 +1,187 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Col, Row } from 'react-grid-system';
+import { useForm } from 'react-hook-form';
+import { addCountry, deleteCountry, getAllCountries, updateCountry } from '../../../functions/settings';
 import FormItem from '../../custom-components/form-item/FormItem';
 import Input from '../../custom-components/input';
-
+import { Table, Space } from 'antd';
+import Modal from 'react-modal';
+import { toast } from 'react-toastify';
+import { AiOutlineClose, AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { customStyle } from '../../../constant/tableCustomStyle';
+import { confirmAlert } from 'react-confirm-alert';
 const CountriesComponent = () => {
   const [data, setData] = useState([]);
-  const [countriesInput, setCountriesInput] = useState('');
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedEditInputValue, setSelectedEditInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
-  const createCountries = async () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  const fetchAllData = async () => getAllCountries().then((res) => setData(res.data));
+  const handleAddCountry = async (data) => {
+    addCountry(data?.country)
+      .then((res) => {
+        fetchAllData();
+        toast(`${res.data.country} successfuly created`);
+        reset();
+      })
+      .catch((err) => {
+        toast(err.response.data);
+        reset();
+      });
+  };
+
+  const handleRemoveCountry = async (removedCountryItem) => {
+    deleteCountry(removedCountryItem)
+      .then((res) => {
+        toast(`${res.data.country} succesfully deleted.`);
+        fetchAllData();
+      })
+      .catch((err) => {
+        toast(err);
+      });
+  };
+
+  const handleUpdateCountry = async () => {
+    setModalIsOpen(false);
+    updateCountry(selectedEditInputValue, {
+      country: inputValue,
+    })
+      .then((res) => {
+        toast(`${selectedEditInputValue} succesfully updated with ${res.data.country}`);
+        fetchAllData();
+      })
+      .catch((err) => {
+        toast(err);
+      });
+  };
+
+  const removeConfirmation = (removedCountryItem) => {
+    confirmAlert({
+      title: <div className="border-b "> Confirmation of Delete </div>,
+      childrenElement: () => (
+        <div className="text-md font-medium ">
+          Are you sure you want to delete <span className="!text-red-500"> {removedCountryItem} ?</span>
+        </div>
+      ),
+      buttons: [
+        { label: 'Cancel', onClick: () => {} },
+        {
+          label: 'Yes, Delete',
+          onClick: () => handleRemoveCountry(removedCountryItem),
+          className: '!bg-[#ffd100] !text-black',
+        },
+      ],
+    });
+  };
+
+  const handleEdit = (selected) => {
+    setModalIsOpen(true);
+    setInputValue(selected);
+    setSelectedEditInputValue(selected);
+  };
+
+  const columns = [
+    {
+      title: 'Countries',
+      dataIndex: 'country',
+      key: 'country',
+      render: (text) => <h6>{text}</h6>,
+    },
+
+    {
+      title: 'Action',
+      key: 'action',
+      render: (selected) => (
+        <Space size="middle">
+          <div className="cursor-pointer">
+            <AiOutlineEdit onClick={() => handleEdit(selected?.country)} size={17} className="text-green-500" />
+          </div>
+          <div>I</div>
+          <div className="cursor-pointer">
+            <AiOutlineDelete onClick={() => removeConfirmation(selected?.country)} size={17} className="text-red-500" />
+          </div>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <>
-      <div className='px-6'>
-        <Row gutterWidth={16}>
-          <Col lg={6}>
-            <FormItem label='Countries'>
-              <Input
-                autoComplete='off'
-                placeholder='Enter a countries'
-                id='countries'
-                name='countries'
-                type='text'
-                value={countriesInput}
-                onChange={(e) => setCountriesInput(e.target.value)}
-              />
-            </FormItem>
-          </Col>
-          <Col lg={6}>
-            <button
-              type='submit'
-              onClick={createCountries}
-              className=' mt-5 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
-            >
-              Add
-            </button>
-          </Col>
-        </Row>
+      <div className="px-6">
+        <form onSubmit={handleSubmit(handleAddCountry)}>
+          <Row gutterWidth={16}>
+            <Col lg={6}>
+              <FormItem label="Country" error={errors?.country}>
+                <Input
+                  autoComplete="off"
+                  placeholder="Enter a country"
+                  id="country"
+                  name="country"
+                  type="text"
+                  {...register('country', { required: ' required field' })}
+                />
+              </FormItem>
+            </Col>
+            <Col lg={6}>
+              <button className=" mt-5 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                Add
+              </button>
+            </Col>
+          </Row>
+        </form>
       </div>
-
-      <div className='flex flex-col'>
-        <div className=' overflow-x-auto sm:-mx-2 lg:-mx-2'>
-          <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-            <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
-              <table className='min-w-full divide-y divide-gray-200'>
-                <thead className='bg-gray-50'>
-                  <tr>
-                    <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                      Countries
-                    </th>
-
-                    <th scope='col' className='px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                      Actions
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className='bg-white divide-y divide-gray-200 w-1/2'>
-                  {/* {shipmentTypes &&
-                        shipmentTypes.map((ship, i) => (
-                          <tr key={i}>
-                            <td className='px-6 py-4 whitespace-nowrap'>
-                              <div className='flex items-center'>
-                                <div className='ml-4'>
-                                  <div className='text-sm font-medium text-gray-900'>{ship?.shipmentType}</div>
-                                </div>
-                              </div>
-                            </td>
-                            <td className='px-6 py-4 whitespace-nowrap  text-sm font-medium text-center'>
-                              <a className='text-indigo-600 mr-4 hover:text-indigo-900 hover:underline'>Edit</a>
-    
-                              <span> I</span>
-                              <a
-                                onClick={() => remove(ship?.shipmentType)}
-                                href='#'
-                                className='text-indigo-600 ml-4 hover:text-indigo-900 hover:underline'
-                              >
-                                Delete
-                              </a>
-                            </td>
-                          </tr>
-                        ))} */}
-                </tbody>
-              </table>
+      <Table columns={columns} dataSource={data} />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyle}
+        contentLabel="Example Modal"
+        ariaHideApp={false}
+      >
+        <form onSubmit={handleUpdateCountry}>
+          <Row>
+            <div className="!flex !justify-between border-b w-full mb-4 pb-4">
+              <div className="">Update Country</div>
+              <div onClick={() => setModalIsOpen(false)}>
+                <AiOutlineClose size={20} className="text-red-600 cursor-pointer" />{' '}
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
+            <Col md={8} lg={8}>
+              <FormItem label="Country">
+                <Input
+                  required
+                  autoComplete="off"
+                  placeholder="Enter a country"
+                  id="editcountry"
+                  name="editcountry"
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+              </FormItem>
+            </Col>
+            <Col md={3} lg={4}>
+              <button
+                type="submit"
+                className=" mt-5 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              >
+                Update
+              </button>
+            </Col>
+          </Row>
+        </form>
+      </Modal>
     </>
   );
 };
